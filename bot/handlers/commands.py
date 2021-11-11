ece069c42beda4201559514eee2c59620d383389
karlygach <karygachkenenbaeva@gmail.com>
2021-11-11 19:02:41 +0600

    commands handling class created

---
+++ bot/handlers/commands.py
@@ -0,0 +1,17 @@
+from telegram.ext import CommandHandler, CallbackContext
+from telegram import Update
+
+from bot.base_bot import TelegramBot
+
+
+class CommandsHandler(TelegramBot):
+    def __init__(self, dispatcher):
+        super(CommandsHandler, self).__init__()
+        self.dispatcher = dispatcher
+        self.add_handlers()
+
+    def add_handlers(self):
+        self.dispatcher.add_handler(CommandHandler('start', self.start))
+
+    def start(self, update: Update, context: CallbackContext):
+        pass
