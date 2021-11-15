3bdc267f8c81a7206286d1862bd7c77bc846c3b9
karlygach <karygachkenenbaeva@gmail.com>
2021-11-15 20:33:14 +0600

    added '/latest' command handler

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,7 +1,9 @@
 from telegram.ext import CommandHandler, CallbackContext
-from telegram import Update
+from telegram import Update, ForceReply, ReplyKeyboardMarkup, KeyboardButton
 
 from bot.base_bot import TelegramBot
+from bot.sources import ExchangeRateApiSource
+from settings import SOURCE_URL_BASE
 
 
 class CommandsHandler(TelegramBot):
@@ -9,9 +11,20 @@ class CommandsHandler(TelegramBot):
         super(CommandsHandler, self).__init__()
         self.dispatcher = dispatcher
         self.add_handlers()
+        self.source = ExchangeRateApiSource(base_url=SOURCE_URL_BASE)
 
     def add_handlers(self):
         self.dispatcher.add_handler(CommandHandler('start', self.start))
+        self.dispatcher.add_handler(CommandHandler('latest', self.get_latest))
 
     def start(self, update: Update, context: CallbackContext):
-        pass
+        chat_id = update.message.chat.id
+        markup = ReplyKeyboardMarkup(keyboard=[
+            ['/latest'],
+        ])
+        self.send_message(recipient=chat_id, message='hello', keyboard=markup)
+
+    def get_latest(self, update: Update, context: CallbackContext):
+        chat_id = update.message.chat.id
+        latest_rates = self.source.get_latest_info()
+        self.send_message(recipient=chat_id, message=latest_rates)
