0406166fb1e3868a40a6360b15c44280304954cf
karlygach <karygachkenenbaeva@gmail.com>
2021-11-18 19:10:35 +0600

    bot messages separated to another file

---
+++ bot/handlers/messages.py
@@ -0,0 +1,7 @@
+GREETING = (
+    "Hello! I am EcxchangeRateBot! I can quickly convert currencies "
+    "for today and can help you keep abreast of events in the world of currencies!\n "
+    "Here some commands I can complete:"
+    "\n/latest - get latest rates of dollar;"
+    "\n/convert - convert from one currency to another"
+)
